62a69caeb37a5ad0d664c009c0329515308ebb92
Thaiane Braga <thbraga@thoughtworks.com>
2019-11-04 10:27:46 -0300

    Um arquivo com múltiplos componentes

---
+++ src/components/DoisComponentes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ComponenteA = props =>
+    <h1>O Componente A diz: {props.value}</h1>
+
+const ComponenteB = props =>
+    <h1>O Componente B diz: {props.value}</h1>
+
+export { ComponenteA, ComponenteB }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import ReactDom from 'react-dom'
 import React from 'react'
 import PrimeiroComponente from './components/PrimeiroComponente'
+import { ComponenteA as A, ComponenteB as B } from './components/DoisComponentes'
 
 const elemento = document.getElementById('root')
 
 ReactDom.render(
     <div>
         <PrimeiroComponente value='Bom dia!' />
+        <A value='Oi, sou A' />
+        <B value='Oi, aqui é B' />
     </div>
     , elemento)
